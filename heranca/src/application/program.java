e76ce0ff982dd172f16a81be1bc8e538618af3df
leoB <leonardozandrade@gmail.com>
2021-07-11 19:33:20 -0300

    First commit.

---
+++ heranca/src/application/program.java
@@ -0,0 +1,9 @@
+package application;
+
+import classes.BusinessAccount;
+
+public class program {
+    public static void main(String[] args) {
+        BusinessAccount business = new BusinessAccount();
+    }
+}

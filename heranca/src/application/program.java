ef3e5e5b2bbe4f7f565b03d1ea0803606c693568
leoB <leonardozandrade@gmail.com>
2021-07-11 20:57:51 -0300

    Uppcasting e Downcasting

--- heranca/src/application/program.java
+++ heranca/src/application/program.java
@@ -1,9 +1,33 @@
 package application;
 
+import classes.Account;
 import classes.BusinessAccount;
+import classes.SavingAccount;
 
 public class program {
     public static void main(String[] args) {
-        BusinessAccount business = new BusinessAccount();
+        Account acc = new Account(1001, "Pedro", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+        // UPCASTING
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(3 , "Bob", 0.0, 250.0 );
+        //Account acc3 = new SavingAccount(5, "Perez", 0.0, 100.0);
+        Account acc3 = new BusinessAccount(3 , "Bob", 0.0, 250.0 );
+
+        // DOWNCASTING
+        BusinessAccount acc4 =  (BusinessAccount)acc2; // cast
+        acc4.loan(100.0);
+
+        if(acc3 instanceof BusinessAccount) {
+            BusinessAccount acc5 = (BusinessAccount)acc3;
+            acc5.loan(100.0);
+            System.out.println("Loan !");
+        }
+        if(acc3 instanceof SavingAccount){
+            SavingAccount acc5 = (SavingAccount)acc3;
+            acc5.updateBalance();
+            System.out.println("Update");
+        }
     }
 }
--- heranca/src/classes/BusinessAccount.java
+++ heranca/src/classes/BusinessAccount.java
@@ -3,13 +3,13 @@ package classes;
 public class BusinessAccount extends Account {
     private Double loanLimit;
 
-    public BusinessAccount(){
-        super();
-    }
-
     public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
         super(number, holder, balance);
         this.loanLimit = loanLimit;
+    }
+
+    public BusinessAccount(){
+        super();
     }
 
     public Double getLoanLimit() {

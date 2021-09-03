99236bfbb5a197ebc3599eafac8a0b30bcf17363
leoB <leonardozandrade@gmail.com>
2021-09-02 21:36:53 -0300

    Recuperando dados.

--- JDBCRules/src/main/java/Main.java
+++ JDBCRules/src/main/java/Main.java
@@ -1,12 +1,27 @@
 import db.DB;
+import db.DBExceptions;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        Connection conn = DB.getConn();
-        DB.CloseConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            conn = DB.getConn();
+            st = conn.createStatement();
+            rs =  st.executeQuery("select * from department");
+            while (rs.next()){
+                System.out.println(rs.getInt("Id") + " "+rs.getString("Name"));
+            }
+        } catch (SQLException throwables) {
+            new DBExceptions(throwables.getMessage());
+        }finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+            DB.CloseConnection();
+        }
+
     }
 }
--- JDBCRules/src/main/java/db/DB.java
+++ JDBCRules/src/main/java/db/DB.java
@@ -1,11 +1,8 @@
 package db;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -41,6 +38,26 @@ public class DB {
             return props;
         } catch (IOException e) {
             throw new DBExceptions(e.getMessage());
+        }
+    }
+
+    public static void closeStatement(Statement st){
+        if(st != null){
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DBExceptions(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs){
+        if(rs != null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DBExceptions(e.getMessage());
+            }
         }
     }
 }

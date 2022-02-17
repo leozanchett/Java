c25c30f8e34a83c516f847d4e15aa6a22961b169
Leo <45577227+leozanchett@users.noreply.github.com>
2022-02-17 14:51:00 -0300

    PadraoBuilder

---
+++ Java Efetivo/Criar e destruir objetos/src/main/java/classes/PadraoBuilder.java
@@ -0,0 +1,60 @@
+package classes;
+
+public class PadraoBuilder {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder{
+        // Exige parâmetros
+        private final int servingSize;
+        private final int servings;
+
+        // Parâmetros opcionais - inicializando para os valores padrão
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrate = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int val){
+            calories = val;
+            return this;
+        }
+
+        public Builder fat(int val){
+            fat = val;
+            return this;
+        }
+
+        public Builder sodium(int val){
+            sodium = val;
+            return this;
+        }
+
+        public Builder carbohydrate(int val){
+            carbohydrate = val;
+            return this;
+        }
+
+        public PadraoBuilder build(){
+            return new PadraoBuilder(this);
+        }
+    }
+
+    private PadraoBuilder(Builder builder){
+        servingSize = builder.servingSize;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbohydrate = builder.carbohydrate;
+    }
+}
--- Java Efetivo/Criar e destruir objetos/src/main/java/main.java
+++ Java Efetivo/Criar e destruir objetos/src/main/java/main.java
@@ -1,4 +1,5 @@
 import classes.JavaBeans;
+import classes.PadraoBuilder;
 import classes.TelescopingConstructor;
 
 public class main {
@@ -12,6 +13,6 @@ public class main {
         javaBeans.setServings(240);
         javaBeans.setServings(8);
         // ----------------------------------------
-                
+        PadraoBuilder padraoBuilder = new PadraoBuilder.Builder(240, 8).calories(100).sodium(35).carbohydrate(27).build();
     }
 }

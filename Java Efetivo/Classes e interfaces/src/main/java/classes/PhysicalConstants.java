6aee7558bc23ddb13672cfdcc0a9e103976b83c0
Leo <45577227+leozanchett@users.noreply.github.com>
2022-02-21 10:21:53 -0300

    Exemplo de classe utilitária

---
+++ Java Efetivo/Classes e interfaces/src/main/java/classes/PhysicalConstants.java
@@ -0,0 +1,11 @@
+package classes;
+
+
+// Exemplo de classe utilitária
+public class PhysicalConstants {
+
+    private PhysicalConstants(){} // Evita instanciação
+
+    public static final double AVOGRADOS_NUMBER = 6.002_140_857e23;
+    public static final double BOLTZMANN_CONST = 1.380_648_52e-23;
+}

4ab015907e55c75b05a6523faac6ea1c4fbb2541
Leo <45577227+leozanchett@users.noreply.github.com>
2022-02-22 15:37:17 -0300

    Enum com métodos específicos para constantes

--- Java Efetivo/Enums/src/main/java/main.java
+++ Java Efetivo/Enums/src/main/java/main.java
@@ -39,6 +39,58 @@ public class main {
         }
     }
 
+    // tipo de enum que faz switch do próprio valor, "questionavel";
+    public enum Operation {
+        PLUS, MINUS, TIMES, DIVIDE;
+        // Operações aritméticas representada pelas constantes
+        public double apply(double x, double y) throws Exception {
+            switch (this){
+                case PLUS -> {
+                    return x + y;
+                }
+                case MINUS -> {
+                    return  x - y;
+                }
+                case TIMES -> {
+                    return x * y;
+                }
+                case DIVIDE -> {
+                    return x / y;
+                }
+            }
+            throw new Exception("Unknown op: "+ this);
+        }
+    }
+
+    // Tipo enum com implementações de métodos especificos para constantes
+    public enum ImpOperation{
+        PLUS {
+            @Override
+            public double apply(double x, double y) {
+                return x + y;
+            }
+        },
+        MINUS {
+            @Override
+            public double apply(double x, double y) {
+                return x - y;
+            }
+        },
+        TIMES {
+            @Override
+            public double apply(double x, double y) {
+                return x * y;
+            }
+        },
+        DIVIDE {
+            @Override
+            public double apply(double x, double y) {
+                return x / y;
+            }
+        };
+        public abstract double apply(double x, double y);
+    }
+
 
     public static void main(String[] args){
         double earthWeigth =  23.54;//Double.parseDouble(args[0]);
@@ -46,6 +98,7 @@ public class main {
         for (Planet p : Planet.values()){
             System.out.printf("Weigth on %s is %f%n", p, p.surfaceWeight(mass));
         }
+        System.out.println(ImpOperation.MINUS.apply(8, 3));
     }
 
 }

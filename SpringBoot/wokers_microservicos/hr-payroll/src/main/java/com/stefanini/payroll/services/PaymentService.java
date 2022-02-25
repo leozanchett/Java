6d7dedb02935ead7293fa5d3ed3382d0ec1e0b1e
Leo <45577227+leozanchett@users.noreply.github.com>
2022-02-25 15:43:02 -0300

    Feign client

--- SpringBoot/wokers_microservicos/hr-payroll/src/main/java/com/stefanini/payroll/services/PaymentService.java
+++ SpringBoot/wokers_microservicos/hr-payroll/src/main/java/com/stefanini/payroll/services/PaymentService.java
@@ -1,29 +1,20 @@
 package com.stefanini.payroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.stefanini.payroll.entities.Payment;
 import com.stefanini.payroll.entities.Worker;
+import com.stefanini.payroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
-	
-	@Value("${hr-worker.host}") // pega a informação que tem origem em application.properties
-	private String workerHost;
-	
+		
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", String.valueOf(workerId));		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);		
+		Worker worker = workerFeignClient.findByID(workerId).getBody();		
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }

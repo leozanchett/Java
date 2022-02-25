435a5b35a0a6e8f751b623da30ff7b513fd50f99
Leo <45577227+leozanchett@users.noreply.github.com>
2022-02-25 15:08:18 -0300

    Rest template

--- SpringBoot/wokers_microservicos/hr-payroll/src/main/java/com/stefanini/payroll/services/PaymentService.java
+++ SpringBoot/wokers_microservicos/hr-payroll/src/main/java/com/stefanini/payroll/services/PaymentService.java
@@ -1,13 +1,29 @@
 package com.stefanini.payroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.stefanini.payroll.entities.Payment;
+import com.stefanini.payroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${hr-worker.host}") // pega a informação que tem origem em application.properties
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", String.valueOf(workerId));		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }

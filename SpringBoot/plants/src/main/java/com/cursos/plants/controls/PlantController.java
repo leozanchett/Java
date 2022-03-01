f5069b7b2099989cc4f9002bb7805aaa43f97942
Leo <45577227+leozanchett@users.noreply.github.com>
2022-03-01 17:26:30 -0300

    Controller methods

--- SpringBoot/plants/src/main/java/com/cursos/plants/controls/PlantController.java
+++ SpringBoot/plants/src/main/java/com/cursos/plants/controls/PlantController.java
@@ -1,4 +1,30 @@
 package com.cursos.plants.controls;
 
+import com.cursos.plants.entities.Plant;
+import com.cursos.plants.repository.PlantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
 public class PlantController {
+
+    @Autowired
+    private PlantRepository plantRepository;
+
+    public PlantController() {
+    }
+
+    @GetMapping("/plants")
+    public Iterable<Plant> getAllPlants(){
+        return  plantRepository.findAll();
+    }
+
+    @GetMapping("/plants/{id}")
+    public Optional<Plant> getPlantById(@PathVariable("id") Integer id){
+        return plantRepository.findById(id);
+    }
 }

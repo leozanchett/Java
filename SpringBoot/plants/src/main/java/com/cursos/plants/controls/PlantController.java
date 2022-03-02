0dee18e9dca60faebe60deef50b5aac185e9dd0a
Leo <45577227+leozanchett@users.noreply.github.com>
2022-03-02 20:55:21 -0300

    @PutMapping

--- SpringBoot/plants/src/main/java/com/cursos/plants/controls/PlantController.java
+++ SpringBoot/plants/src/main/java/com/cursos/plants/controls/PlantController.java
@@ -5,6 +5,7 @@ import com.cursos.plants.repository.PlantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.Optional;
 
 @RestController
@@ -29,5 +30,28 @@ public class PlantController {
     @PostMapping("/plants")
     public Plant createNewPlant(@RequestBody Plant plant){
         return plantRepository.save(plant);
+    }
+
+    @PutMapping("/plants/{id}")
+    public Plant updatePlant(@PathVariable("id") Integer id, @RequestBody Plant p){
+        Optional<Plant> plantToUpdateOptional = plantRepository.findById(id);
+        if(plantToUpdateOptional.isPresent()){
+            Plant plantToUpdate = plantToUpdateOptional.get();
+            if(p.getName() != null){
+                plantToUpdate.setName(p.getName());
+            }
+            if(p.getHasFruit() != null){
+                plantToUpdate.setHasFruit(p.getHasFruit());
+            }
+            if(p.getQuantity() != null){
+                plantToUpdate.setQuantity(p.getQuantity());
+            }
+            if(p.getWateringFrequency() != null){
+                plantToUpdate.setWateringFrequency(p.getWateringFrequency());
+            }
+            return plantRepository.save(plantToUpdate);
+        }else{
+            return null;
+        }
     }
 }

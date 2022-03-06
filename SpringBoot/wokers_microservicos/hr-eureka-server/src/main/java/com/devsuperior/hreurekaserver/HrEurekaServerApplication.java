43693fc632b4c8f96ba1e2de044c6071ee7ba95c
Leo <45577227+leozanchett@users.noreply.github.com>
2022-03-06 17:24:32 -0300

    hr-eureka-server container

--- SpringBoot/wokers_microservicos/hr-eureka-server/src/main/java/com/devsuperior/hreurekaserver/HrEurekaServerApplication.java
+++ SpringBoot/wokers_microservicos/hr-eureka-server/src/main/java/com/devsuperior/hreurekaserver/HrEurekaServerApplication.java
@@ -1,11 +1,11 @@
-package com.stefanini.hreurekaserver;
+package com.devsuperior.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
+@SpringBootApplication
 public class HrEurekaServerApplication {
 
 	public static void main(String[] args) {
--- SpringBoot/wokers_microservicos/hr-eureka-server/src/test/java/com/devsuperior/hreurekaserver/HrEurekaServerApplicationTests.java
+++ SpringBoot/wokers_microservicos/hr-eureka-server/src/test/java/com/devsuperior/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.stefanini.hreurekaserver;
+package com.devsuperior.hreurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

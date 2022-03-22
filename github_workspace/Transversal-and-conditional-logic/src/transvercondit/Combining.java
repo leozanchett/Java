31588a8180218cbcf858ff9e8890ee13bb974948
Leo <45577227+leozanchett@users.noreply.github.com>
2022-03-22 14:19:29 -0300

    Traversal and Conditional Logic

---
+++ github_workspace/Transversal-and-conditional-logic/src/transvercondit/Combining.java
@@ -0,0 +1,45 @@
+package transvercondit;
+
+import java.util.Arrays;
+
+public class Combining {
+	public static void main(String[] args) {
+		int[][] imageData = { 
+				{ 100, 90, 255, 80, 70, 255, 60, 50 }, 
+				{ 255, 10, 5, 255, 10, 5, 255, 255 },
+				{ 255, 255, 255, 0, 255, 255, 255, 75 }, 
+				{ 255, 60, 30, 0, 30, 60, 255, 255 } };
+
+		// First, we want to crop the image down to a 4x6 image, removing the right 2
+		// columns. Declare and initialize a new 2D array of integers with 4 rows and 6
+		// columns called `newImage`.
+		int[][] newImage = new int[4][6];
+
+		// Now that you have your empty image, use nested **for** loops to copy over the
+		// data from the original image to the new image, make sure not to include the
+		// cropped out columns.
+		for (int outer = 0; outer < imageData.length; outer++) {
+			for (int inner = 0; inner < imageData[outer].length - 2; inner++) {
+				newImage[outer][inner] = imageData[outer][inner];
+			}
+		}
+
+		System.out.println(Arrays.deepToString(newImage));
+
+		// You want to decrease the brightness of the new image by 50 units. The way
+		// this works is that for every integer in the new 2D array, we will subtract
+		// the value by 50. Remember that the value range for the pixel is 0-255, so if
+		// the result tries to go below 0, just set it equal to 0.
+		for (int outer = 0; outer < newImage.length; outer++) {
+			for (int inner = 0; inner < newImage[outer].length; inner++) {
+				if (newImage[outer][inner] != 0 && newImage[outer][inner] - 50 > 0) {
+					newImage[outer][inner] = newImage[outer][inner] - 50;
+				} else {
+					newImage[outer][inner] = 0;
+				}
+			}
+		}
+
+		System.out.println(Arrays.deepToString(newImage));
+	}
+}

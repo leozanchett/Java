2fd1233365355bec696739be7e7e2d2830742ce8
Leo <45577227+leozanchett@users.noreply.github.com>
2022-03-31 22:42:14 -0300

    Hamcrest

--- JUnit/tdd/src/main/java/com/junittestes/tdd/Calculator.java
+++ JUnit/tdd/src/main/java/com/junittestes/tdd/Calculator.java
@@ -13,4 +13,8 @@ public class Calculator {
    public Object divide(int i, int j) {
       return i / j;
    }
+
+   public Object subtract(int i, int j) {
+      return i - j;
+   }
 }
---
+++ JUnit/tdd/src/test/java/com/junittestes/tdd/HamcrestAssertionsDemo.java
@@ -0,0 +1,16 @@
+package com.junittestes.tdd;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class HamcrestAssertionsDemo {
+   private final Calculator calculator = new Calculator();
+
+   @Test
+   void assertWithHamcrestMatcher() {
+      assertThat(calculator.subtract(4, 1), is(equalTo(3)));
+   }
+}

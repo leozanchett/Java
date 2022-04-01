f0add1f5ec086f6f634cb16ccc7c4ff7725c80b0
Leo <45577227+leozanchett@users.noreply.github.com>
2022-03-31 22:36:56 -0300

    Updates test

--- JUnit/tdd/src/main/java/com/junittestes/tdd/Calculator.java
+++ JUnit/tdd/src/main/java/com/junittestes/tdd/Calculator.java
@@ -1,7 +1,16 @@
 package com.junittestes.tdd;
 
 public class Calculator {
+
    public int multiply(int a, int b) {
       return a * b;
+   }
+
+   public Integer add(int i, int j) {
+      return i + j;
+   }
+
+   public Object divide(int i, int j) {
+      return i / j;
    }
 }
--- JUnit/tdd/src/test/java/com/junittestes/tdd/CalculatorTest.java
+++ JUnit/tdd/src/test/java/com/junittestes/tdd/CalculatorTest.java
@@ -1,11 +1,17 @@
 package com.junittestes.tdd;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.time.Duration;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,10 +21,12 @@ import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
    Calculator calculator;
+   Person person;
 
    @BeforeEach
    void setUp() {
       calculator = new Calculator();
+      person = new Person("John", "Doe");
    }
 
    @Test
@@ -40,6 +48,62 @@ public class CalculatorTest {
    @Tag("fast")
    @Test
    public @interface FastTest {
-      
+
+   }
+
+   @Test
+   void standardAssertions() {
+      assertEquals(2, calculator.add(1, 1));
+      assertEquals(4, calculator.multiply(2, 2), "The optional failure message is now the last parameter");
+      assertTrue('a' < 'b',
+            () -> "Assertion messages can be lazily evaluated --  to avoid constructing complex messages unnecessarily.");
+   }
+
+   @Test
+   void groupedAssertions() {
+      // In a grouped assertion all assertions are executed, and all
+      // failures will be reported together.
+      assertAll("O nome e sobrenome das pessoas devem ser iguais",
+            () -> assertEquals("John", person.getName()),
+            () -> assertEquals("Doe", person.getLastName()));
+   }
+
+   @Test
+   void dependentAssertions() {
+      // Within a code block, if an assertion fails the
+      // subsequent code in the same block will be skipped.
+      assertAll("valor das properties",
+            () -> {
+               String firstName = person.getName();
+               assertNotNull(firstName);
+               // Executed only if the previous assertion is valid.
+               assertAll("first name",
+                     () -> assertTrue(firstName.startsWith("J")),
+                     () -> assertTrue(firstName.endsWith("n")));
+            },
+            () -> {
+               // Grouped assertion, so processed independently
+               // of results of first name assertions.
+               String lastName = person.getLastName();
+               assertNotNull(lastName);
+               // Executed only if the previous assertion is valid.
+               assertAll("last name",
+                     () -> assertTrue(lastName.startsWith("D")),
+                     () -> assertTrue(lastName.endsWith("e")));
+            });
+   }
+
+   @Test
+   void exceptionTesting() {
+      Exception exception = assertThrows(ArithmeticException.class, () -> calculator.divide(1, 0));
+      assertEquals("/ by zero", exception.getMessage());
+   }
+
+   @Test
+   void timeoutNotExceeded() {
+      // The following assertion succeeds.
+      assertTimeout(Duration.ofMinutes(2), () -> {
+         // Perform task that takes less than 2 minutes.
+      });
    }
 }

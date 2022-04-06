e857c70b679ee7cebadbc483415f7d400db1c1f8
Leo <45577227+leozanchett@users.noreply.github.com>
2022-04-06 10:31:45 -0300

    Configuration file

---
+++ SpringBoot-News/spring-demo-one/src/main/java/com/example/demo/HelloSpringApp.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+   public static void main(String[] args) {
+      // load the spring configuration file
+      ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+      // retrieve bean from sprint container
+      Coach theCoach = context.getBean("myCoach", Coach.class);
+
+      // call methos on the bean
+      System.out.println(theCoach.getDailyWorkout());
+
+      // close the context
+      context.close();
+
+   }
+
+}

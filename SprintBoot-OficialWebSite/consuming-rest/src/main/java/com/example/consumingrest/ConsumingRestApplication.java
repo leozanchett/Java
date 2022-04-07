488e734a61cd3b6c860f87a3096a9ae8c2bfbce3
Leo <45577227+leozanchett@users.noreply.github.com>
2022-04-07 13:22:23 -0300

    Consuming RestApllication

---
+++ SprintBoot-OficialWebSite/consuming-rest/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -0,0 +1,35 @@
+package com.example.consumingrest;
+
+import org.slf4j.Logger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ConsumingRestApplication {
+
+	private static final Logger log = org.slf4j.LoggerFactory.getLogger(ConsumingRestApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			//String url = "https://quoters.apps.pcfone.io/api/random";
+			String url = "https://viacep.com.br/ws/01001000/json/";
+			log.info("REST call: " + url);
+			ViaCep quote = restTemplate.getForObject(url, ViaCep.class);
+			log.info("Result: " + quote.toString());
+		};
+	}
+}

4aad8c1737a2b328b23cf31ca60122498a6fa8fb
Leo <45577227+leozanchett@users.noreply.github.com>
2022-11-24 14:45:50 -0300

    multiple_type_parameters

--- GenericsAndCollections/src/generic_interfaces/Main.java
+++ GenericsAndCollections/src/generic_interfaces/Main.java
@@ -2,6 +2,10 @@ package generic_interfaces;
 
 public class Main {
 
+    public static <T> boolean isDouble(T object) {
+        return object instanceof Double;
+    }
+
     public static void main(String[] args) {
         int myNumber = 24;
         String bookName = "Hello Book!";
@@ -11,5 +15,12 @@ public class Main {
 
         System.out.println(containerRetriever.retrieveData());
         System.out.println(bookRetriever.retrieveData());
+
+        String test1 = "Double";
+        double test2 = 5.8;
+        final boolean isTest1Double = isDouble(test1);
+        final boolean isTest2Double = isDouble(test2);
+        System.out.println(isTest1Double);
+        System.out.println(isTest2Double);
     }
 }
